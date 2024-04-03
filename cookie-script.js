d4d0c15add0d2ff5f23054981d4b73517cd78f11
UpReach2016 <110052888+UpReach2016@users.noreply.github.com>
2024-04-03 13:04:46 +0200

    Delete cookie-script.js

--- cookie-script.js
+++
@@ -1,4 +0,0 @@
-// Cookie Script
-var cookieScript = document.createElement("script");
-cookieScript.src = "//cdn.cookie-script.com/s/a37524ffb903e19aaa7b80c13ad7320b.js";
-document.head.appendChild(cookieScript);
