a0767a460608748a8e74a21a3fbfa942d68b2ab1
UpReach2016 <110052888+UpReach2016@users.noreply.github.com>
2024-04-03 13:08:15 +0200

    Add files via upload

---
+++ cookie-script.js
@@ -0,0 +1,4 @@
+// Cookie Script
+var cookieScript = document.createElement("script");
+cookieScript.src = "//cdn.cookie-script.com/s/a37524ffb903e19aaa7b80c13ad7320b.js";
+document.head.appendChild(cookieScript);
